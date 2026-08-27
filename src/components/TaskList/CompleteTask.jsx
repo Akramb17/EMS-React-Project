2a26aa084b0556604eb79cba84ce1ec8115739a7
Akramb17 <akrambhura1701@gmail.com>
2026-08-27 20:48:23 +0530

    Implement dynamic task data

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 
-const CompleteTask = () => {
+const CompleteTask = ({data}) => {
   return (
     <div className='shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
         <div className='flex justify-between items-center'>
-          <h3 className='bg-red-500 text-sm py-1 rounded'>High</h3>
-          <h4 className='text-sm'>20 feb 2024</h4>
+          <h3 className='bg-red-500 text-sm py-1 rounded'>{data.category}</h3>
+          <h4 className='text-sm'>{data.date}</h4>
         </div>
-        <h2 className='mt-5 text-2xl font-semibold '>Make a youtube video</h2>
-        <p className='text-sm mt-2'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, cumque eaque nesciunt natus ipsum enim atque nobis amet deleniti unde soluta ut minima architecto animi nulla aut, dolorem possimus porro?
-        </p>
+        <h2 className='mt-5 text-2xl font-semibold '>{data.title}</h2>
+        <p className='text-sm mt-2'>{data.description}</p>
         <div className='mt-6 '>
           <button className='className= w-full bg-green-600 rounded font-medium py-1 px-2 text-xs'>Completed</button>
         </div>
